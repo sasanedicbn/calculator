b6999a1ce8dbec36a762267c1f8a556a029ab559
Sasa Nedic <svetasmak0@gmail.com>
2024-01-05 15:41:11 +0100

    Create divide function

--- script.js
+++ script.js
@@ -15,11 +15,17 @@ function calculator() {
     currentResult *= number;
     console.log(`Result of subtract: ${currentResult}`);
   }
-
+  function divide(number) {
+    if (number !== 0) {
+      currentResult /= number;
+    }
+    console.log(`Result of subtract: ${currentResult}`);
+  }
   return {
     add,
     subtract,
     multiply,
+    divide,
   };
 }
 const logic = calculator();
