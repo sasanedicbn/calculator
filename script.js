00c5db8c853099a342fa9835d8877c062a1037b9
Sasa Nedic <svetasmak0@gmail.com>
2024-01-05 15:32:59 +0100

    Create calculator function with add() function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const buttons = document.querySelectorAll("button");
+function calculator() {
+  let number1;
+  let currentResult = 0;
+  let operation;
+  function add(number) {
+    currentResult += number;
+    console.log(`Result of add: ${currentResult}`);
+  }
+
+  return {
+    add,
+  };
+}
+const logic = calculator();
+
+logic.add(10);
