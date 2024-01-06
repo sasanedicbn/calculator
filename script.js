e6fb51c6138bfd58fd78e422d8f02ea78ed0691a
Sasa Nedic <svetasmak0@gmail.com>
2024-01-06 16:55:43 +0100

    Change logic

--- script.js
+++ script.js
@@ -1,44 +1,7 @@
 const buttons = document.querySelectorAll("button");
 function calculator() {
-  let currentResult = 0;
-  let operation;
-  function add(...numbers) {
-    for (const number of numbers) {
-      currentResult += number;
-    }
-    return currentResult;
-  }
-  function subtract(...numbers) {
-    console.log(...numbers);
-    for (const number of numbers) {
-      currentResult -= number;
-    }
-    return currentResult;
-  }
-  function multiply(...numbers) {
-    currentResult = 1;
-    console.log(numbers);
-    for (const number of numbers) {
-      currentResult *= number;
-    }
-    return currentResult;
-  }
-  function divide(...numbers) {
-    for (const number of numbers) {
-      if (number !== 0) {
-        currentResult /= number;
-      }
-    }
-    return currentResult;
-  }
-  return {
-    add,
-    subtract,
-    multiply,
-    divide,
-  };
+  let firstNumber = "";
+  let secondNubmer = "";
+  let operation = "";
+  let result = 0;
 }
-const logic = calculator();
-
-const result = logic.subtract(10, 3);
-console.log(result);
